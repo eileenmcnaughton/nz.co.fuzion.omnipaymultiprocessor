b7038652f0154fd7244c56be53c422a3519485f4
Eileen McNaughton <eileen@fuzion.co.nz>
2014-06-17 11:00:06 +1200

    civix generated shell

---
+++ omnipaymultiprocessor.php
@@ -0,0 +1,70 @@
+<?php
+
+require_once 'omnipaymultiprocessor.civix.php';
+
+/**
+ * Implementation of hook_civicrm_config
+ */
+function omnipaymultiprocessor_civicrm_config(&$config) {
+  _omnipaymultiprocessor_civix_civicrm_config($config);
+}
+
+/**
+ * Implementation of hook_civicrm_xmlMenu
+ *
+ * @param $files array(string)
+ */
+function omnipaymultiprocessor_civicrm_xmlMenu(&$files) {
+  _omnipaymultiprocessor_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implementation of hook_civicrm_install
+ */
+function omnipaymultiprocessor_civicrm_install() {
+  return _omnipaymultiprocessor_civix_civicrm_install();
+}
+
+/**
+ * Implementation of hook_civicrm_uninstall
+ */
+function omnipaymultiprocessor_civicrm_uninstall() {
+  return _omnipaymultiprocessor_civix_civicrm_uninstall();
+}
+
+/**
+ * Implementation of hook_civicrm_enable
+ */
+function omnipaymultiprocessor_civicrm_enable() {
+  return _omnipaymultiprocessor_civix_civicrm_enable();
+}
+
+/**
+ * Implementation of hook_civicrm_disable
+ */
+function omnipaymultiprocessor_civicrm_disable() {
+  return _omnipaymultiprocessor_civix_civicrm_disable();
+}
+
+/**
+ * Implementation of hook_civicrm_upgrade
+ *
+ * @param $op string, the type of operation being performed; 'check' or 'enqueue'
+ * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
+ *
+ * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
+ *                for 'enqueue', returns void
+ */
+function omnipaymultiprocessor_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _omnipaymultiprocessor_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implementation of hook_civicrm_managed
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ */
+function omnipaymultiprocessor_civicrm_managed(&$entities) {
+  return _omnipaymultiprocessor_civix_civicrm_managed($entities);
+}
