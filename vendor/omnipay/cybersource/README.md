df4f08dd6267cae759706a06add5604a40f59969
Eileen McNaughton <eileen@fuzion.co.nz>
2014-07-14 20:38:41 +1200

    in prog

---
+++ vendor/omnipay/cybersource/README.md
@@ -0,0 +1,2 @@
+omnipay-cybersource
+===================
---
+++ vendor/omnipay/cybersource/tests/GatewayTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Omnipay\Cybersource;
+
+use Omnipay\Tests\GatewayTestCase;
+
+class GatewayTest extends GatewayTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->gateway = new Gateway($this->getHttpClient(), $this->getHttpRequest());
+    }
+
+    public function testPurchase()
+    {
+        $request = $this->gateway->purchase(array('amount' => '10.00'));
+
+        $this->assertInstanceOf('Omnipay\Cybersource\Message\PurchaseRequest', $request);
+        $this->assertSame('10.00', $request->getAmount());
+    }
+
+    public function testCompletePurchase()
+    {
+        $request = $this->gateway->completePurchase(array('amount' => '10.00'));
+
+        $this->assertInstanceOf('Omnipay\Cybersource\Message\CompletePurchaseRequest', $request);
+        $this->assertSame('10.00', $request->getAmount());
+    }
+
+    public function testFetchTransaction()
+    {
+        $request = $this->gateway->fetchTransaction(array('transactionReference' => 'abc123'));
+
+        $this->assertInstanceOf('Omnipay\Cybersource\Message\FetchTransactionRequest', $request);
+        $this->assertSame('abc123', $request->getTransactionReference());
+    }
+}
