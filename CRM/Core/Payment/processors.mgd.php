6f4bb648fcec153e774ba5db4756446e16fcfaae
Eileen McNaughton <eileen@fuzion.co.nz>
2015-01-05 09:19:18 +1300

    #6 add comments around adding a processor

--- CRM/Core/Payment/processors.mgd.php
+++ CRM/Core/Payment/processors.mgd.php
@@ -4,10 +4,33 @@
  * User: eileen
  * Date: 17/06/2014
  * Time: 10:14 PM
+ *
+ * To add a new processor you need to add an item to this array. It is sequentially numerically indexed and the important aspects are
+ *
+ * - name - omnipay_{Processor_Name}, Omnipay calls the gateway method create with the processor name as the parameter.
+ * To get the processor name take a look at the Readme for the gateway you are adding - you will generally see
+ * The following gateways are provided by this package: Mollie
+ *
+ * - user_name_label, password_label, signature_label, subject_label - these are generally about telling the plugin what to call these when they pass them to
+ * Omnipay. They are also shown to users so some reformatting is done to turn it into lower-first-letter camel case. Take a look at the gateway file for your gateway. This is directly under src.
+ * Some provide more than one and the 'getName' function distinguishes them. The getDefaultParameters will tell you what to pass. eg if you see
+ * 'apiKey' you should enter 'user_name' => 'Api Key' (you might ? be able to get away with 'API Key' - need to check). You can provide as many or as
+ * few as you want of these and it's irrelevant which field you put them in but note that the signature field is the longest and that
+ * in future versions of CiviCRM hashing may be done on password and signature on the screen.
+ *
+ * - 'class_name' => 'Payment_OmnipayMultiProcessor', (always)
+ *
+ * - 'url_site_default' - this is ignored. But, by giving one you make it easier for people adding processors
+ *
+ * - 'billing_mode' - 1 = onsite, 4 = redirect offsite (including transparent redirects).
+ *
+ * - payment_mode - 1 = credit card, 2 = debit card, 3 = transparent redirect. In practice 3 means that billing details are gathered on-site so
+ * it may also be used with automatic redirects where address fields need to be mandatory for the signature.
+ *
+ * The record will be automatically inserted, updated, or deleted from the
+ * database as appropriate. For more details, see "hook_civicrm_managed" at:
+ * http://wiki.civicrm.org/confluence/display/CRMDOC/Hook+Reference
  */
-// The record will be automatically inserted, updated, or deleted from the
-// database as appropriate. For more details, see "hook_civicrm_managed" at:
-// http://wiki.civicrm.org/confluence/display/CRMDOC/Hook+Reference
 return array(
   /*
    * Omnipay was enabled as a test- but it also requires a js script
@@ -96,30 +119,26 @@ return array(
     ),
   4 =>
     array(
-        'name' => 'OmniPay - GoPay',
-        'entity' => 'payment_processor_type',
-        'params' =>
-            array(
-                'version' => 3,
-                'title' => 'OmniPay - GoPay',
-                'name' => 'omnipay_Gopay',
-                'description' => 'Omnipay GoPay Payment Processor',
-
-                // DO NOT CHANGE: Labels are used as Omnipay gateway properties.
-                'user_name_label' => 'Go Id',
-                'password_label' => 'Secure Key',
-                'signature_label' => '',
-
-                'class_name' => 'Payment_OmnipayMultiProcessor',
-
-                'url_site_default' => 'unused',
-                'url_api_default' => 'unused',
-                'url_site_test_default' => 'unused',
-                'url_api_test_default' => 'unused',
-
-                'billing_mode' => 4,
-                'payment_type' => 1,
-            ),
+      'name' => 'OmniPay - GoPay',
+      'entity' => 'payment_processor_type',
+      'params' =>
+        array(
+          'version' => 3,
+          'title' => 'OmniPay - GoPay',
+          'name' => 'omnipay_Gopay',
+          'description' => 'Omnipay GoPay Payment Processor',
+          // DO NOT CHANGE: Labels are used as Omnipay gateway properties.
+          'user_name_label' => 'Go Id',
+          'password_label' => 'Secure Key',
+          'signature_label' => '',
+          'class_name' => 'Payment_OmnipayMultiProcessor',
+          'url_site_default' => 'unused',
+          'url_api_default' => 'unused',
+          'url_site_test_default' => 'unused',
+          'url_api_test_default' => 'unused',
+          'billing_mode' => 4,
+          'payment_type' => 1,
+        ),
     ),
   /*
   2 =>
---
+++ README.md
@@ -0,0 +1,10 @@
+This extension provides a wrapper extension for payment processors
+
+How to add new payment gateways to Omnipay
+
+
+1) update composer.json and run composer update
+
+
+2) edit CRM/Core/Payment/processors.mgd.php - check notes in that file for syntax
+
