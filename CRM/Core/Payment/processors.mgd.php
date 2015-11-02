f8571b006199502c3d25a3df91f13906949253eb
eileenmcnaugton <eileen@fuzion.co.nz>
2015-11-02 14:51:56 +1300

    reformatting of the mgd file

--- CRM/Core/Payment/processors.mgd.php
+++ CRM/Core/Payment/processors.mgd.php
@@ -63,47 +63,43 @@ return array(
         ),
     ),
   */
-  1 =>
-    array(
-      'name' => 'OmniPay - Cybersource',
-      'entity' => 'payment_processor_type',
-      'params' =>
-        array(
-          'version' => 3,
-          'title' => 'OmniPay - Cybersource',
-          'name' => 'omnipay_Cybersource',
-          'description' => 'Omnipay Cybersource Payment Processor',
-          'user_name_label' => 'Profile ID',
-          'password_label' => 'Access Key',
-          'signature_label' => 'Secret Key',
-          'class_name' => 'Payment_OmnipayMultiProcessor',
-          'url_site_default' => 'https://testsecureacceptance.cybersource.com/silent/pay',
-          'url_api_default' => 'https://testsecureacceptance.cybersource.com/silent/pay',
-          'billing_mode' => 4,
-          'payment_type' => 3,
-        ),
+  1 => array(
+    'name' => 'OmniPay - Cybersource',
+    'entity' => 'payment_processor_type',
+    'params' => array(
+      'version' => 3,
+      'title' => 'OmniPay - Cybersource',
+      'name' => 'omnipay_Cybersource',
+      'description' => 'Omnipay Cybersource Payment Processor',
+      'user_name_label' => 'Profile ID',
+      'password_label' => 'Access Key',
+      'signature_label' => 'Secret Key',
+      'class_name' => 'Payment_OmnipayMultiProcessor',
+      'url_site_default' => 'https://testsecureacceptance.cybersource.com/silent/pay',
+      'url_api_default' => 'https://testsecureacceptance.cybersource.com/silent/pay',
+      'billing_mode' => 4,
+      'payment_type' => 3,
     ),
-  2 =>
-    array(
-      'name' => 'OmniPay - Paybox System',
-      'entity' => 'payment_processor_type',
-      'params' =>
-        array(
-          'version' => 3,
-          'title' => 'OmniPay - PayboxSystem',
-          'name' => 'omnipay_Paybox_System',
-          'description' => 'Omnipay Paybox Payment Processor',
-          'user_name_label' => 'Site',
-          'password_label' => 'Identifiant',
-          'signature_label' => 'Key',
-          'subject_label' => 'Rang',
-          'class_name' => 'Payment_OmnipayMultiProcessor',
-          'url_site_default' => 'https://dummyurl.com',
-          'url_api_default' => 'https://dummyurl.com',
-          'billing_mode' => 4,
-          'payment_type' => 1,
-        ),
+  ),
+  2 => array(
+    'name' => 'OmniPay - Paybox System',
+    'entity' => 'payment_processor_type',
+    'params' => array(
+      'version' => 3,
+      'title' => 'OmniPay - PayboxSystem',
+      'name' => 'omnipay_Paybox_System',
+      'description' => 'Omnipay Paybox Payment Processor',
+      'user_name_label' => 'Site',
+      'password_label' => 'Identifiant',
+      'signature_label' => 'Key',
+      'subject_label' => 'Rang',
+      'class_name' => 'Payment_OmnipayMultiProcessor',
+      'url_site_default' => 'https://dummyurl.com',
+      'url_api_default' => 'https://dummyurl.com',
+      'billing_mode' => 4,
+      'payment_type' => 1,
     ),
+  ),
   /*3 =>
     array(
       'name' => 'OmniPay - Paybox Direct',
@@ -125,29 +121,27 @@ return array(
         ),
     ),
   */
-  4 =>
-    array(
-      'name' => 'OmniPay - GoPay',
-      'entity' => 'payment_processor_type',
-      'params' =>
-        array(
-          'version' => 3,
-          'title' => 'OmniPay - GoPay',
-          'name' => 'omnipay_Gopay',
-          'description' => 'Omnipay GoPay Payment Processor',
-          // DO NOT CHANGE: Labels are used as Omnipay gateway properties.
-          'user_name_label' => 'Go Id',
-          'password_label' => 'Secure Key',
-          'signature_label' => '',
-          'class_name' => 'Payment_OmnipayMultiProcessor',
-          'url_site_default' => 'unused',
-          'url_api_default' => 'unused',
-          'url_site_test_default' => 'unused',
-          'url_api_test_default' => 'unused',
-          'billing_mode' => 4,
-          'payment_type' => 1,
-        ),
+  4 => array(
+    'name' => 'OmniPay - GoPay',
+    'entity' => 'payment_processor_type',
+    'params' => array(
+      'version' => 3,
+      'title' => 'OmniPay - GoPay',
+      'name' => 'omnipay_Gopay',
+      'description' => 'Omnipay GoPay Payment Processor',
+      // DO NOT CHANGE: Labels are used as Omnipay gateway properties.
+      'user_name_label' => 'Go Id',
+      'password_label' => 'Secure Key',
+      'signature_label' => '',
+      'class_name' => 'Payment_OmnipayMultiProcessor',
+      'url_site_default' => 'unused',
+      'url_api_default' => 'unused',
+      'url_site_test_default' => 'unused',
+      'url_api_test_default' => 'unused',
+      'billing_mode' => 4,
+      'payment_type' => 1,
     ),
+  ),
   /*
   5 =>
     array(
