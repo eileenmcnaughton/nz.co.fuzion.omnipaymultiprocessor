1fe18e6d16419525cfb9a2f1b4513cc75cf2f425
eileenmcnaugton <eileen@fuzion.co.nz>
2016-10-24 10:14:45 +1300

    Get processor id from message if not otherwise clear" Gi
    Get processor_id from context if n

--- api/v3/PaymentNotification.php
+++ api/v3/PaymentNotification.php
@@ -10,7 +10,12 @@
 function civicrm_api3_payment_notification_create($params) {
   if (!empty($params['system_log_id'])) {
     // lets replace params with this rather than allow altering
-    $params = json_decode(civicrm_api3('system_log', 'getvalue', array('id' => $params['system_log_id'], 'return' => 'context')), TRUE);
+    $log = civicrm_api3('system_log', 'getsingle', array('id' => $params['system_log_id'], 'return' => 'context, message'));
+    $params = json_decode($log['context'], TRUE);
+  }
+  if (empty($params['processor_id']) && stristr($log['message'], 'processor_id=')) {
+    $parts = explode('processor_id=', $log['message']);
+    $params['processor_id'] = array_pop($parts);
   }
   return civicrm_api3_create_success(CRM_Core_Payment_OmnipayMultiProcessor::processPaymentResponse($params));
 }
