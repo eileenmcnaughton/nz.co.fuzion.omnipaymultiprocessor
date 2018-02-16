bd51f6bf3ec35c2985c4fcc980ff2794f1502cec
eileen <emcnaughton@wikimedia.org>
2018-02-16 14:00:03 +1300

    Start on Notify response

--- vendor/fuzion/omnipay-mercanet/src/Message/OffsiteCompleteAuthorizeResponse.php
+++ vendor/fuzion/omnipay-mercanet/src/Message/OffsiteCompleteAuthorizeResponse.php
@@ -3,12 +3,14 @@ namespace Omnipay\Mercanet\Message;
 
 use Omnipay\Common\Message\AbstractResponse;
 use Omnipay\Common\Message\RequestInterface;
+use Omnipay\Mercanet\Message\OffsiteNotificationTrait;
 
 /**
  * Complete Authorize Response
  */
 class OffsiteCompleteAuthorizeResponse extends AbstractResponse
 {
+    use OffsiteNotificationTrait;
 
     public function __construct(\Omnipay\Common\Message\RequestInterface $request, $data)
     {
@@ -16,44 +18,4 @@ class OffsiteCompleteAuthorizeResponse extends AbstractResponse
         $this->data = $data;
     }
 
-  /**
-     * Check the parameters that have been passed in to determine if the response represents a successful transaction.
-     *
-     * @return bool
-     */
-    public function isSuccessful()
-    {
-        return isset($this->data['responseCode']) && '00' === $this->data['responseCode'];
-    }
-
-    /**
-     * Check response for a transaction reference.
-     *
-     * @return string|null
-     */
-    public function getTransactionReference()
-    {
-        return isset($this->data['authorisationId']) ? $this->data['authorisationId'] : null;
-    }
-
-    /**
-     * check response for a message. Usually these are provide in the event of a failure - e.g a decline
-     * @return string|null
-     */
-    public function getMessage()
-    {
-        return isset($this->data['response_message']) ? $this->data['response_message'] : null;
-    }
-
-    /**
-     * Check response for a transaction ID.
-     *
-     * Most commonly this will be the invoice id.
-     *
-     * @return string|null
-     */
-    public function getTransactionId()
-    {
-        return isset($this->data['transactionReference']) ? $this->data['transactionReference'] : null;
-    }
 }
