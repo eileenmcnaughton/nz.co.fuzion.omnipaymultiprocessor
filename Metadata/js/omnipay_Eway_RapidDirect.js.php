1921c800bd08cd48af349e066975f29fc3b3095b
eileen <emcnaughton@wikimedia.org>
2018-07-30 19:08:03 +1200

    WIP eway Client side encryption

--- Metadata/js/omnipay_Eway_RapidDirect.js.php
+++ Metadata/js/omnipay_Eway_RapidDirect.js.php
@@ -1,7 +1,51 @@
-<?php
 /**
- * Created by IntelliJ IDEA.
- * User: emcnaughton
- * Date: 7/30/18
- * Time: 5:30 PM
+ * Encrypt confidential data fields before the form is submitted.
+ *
+ * @param field
+ * @param string apiKey
  */
+function encryptField(field, apiKey) {
+  var existingValue = field.val();
+  if (existingValue == "") {
+    return;
+  }
+  if (isFieldEncrypted(field)) {
+    return;
+  }
+  field.val(eCrypt.encryptValue(existingValue, apiKey));
+}
+
+/**
+ * Hide fields that have already been encrypted.
+ */
+function hideEncryptedFields(field) {
+  if (!isFieldEncrypted(field)) {
+    return;
+  }
+  field.hide();
+}
+
+/**
+ * Is the field already encrypted.
+ *
+ * @param field
+ */
+function isFieldEncrypted(field) {
+  if (field.length == 0) {
+    return;
+  }
+  var existingValue = field.val();
+  if (existingValue.length != 0 && existingValue.substr(0, 9) == 'eCrypted:') {
+    return true;
+  }
+  return false;
+}
+
+hideEncryptedFields(cj('#credit_card_number'));
+hideEncryptedFields(cj('#cvv2'));
+
+cj(CRM.eway.formSelector).submit(function() {
+    encryptField(cj('#credit_card_number'), CRM.eway.ewayKey);
+    encryptField(cj('#cvv2'), CRM.eway.ewayKey);
+  }
+);
