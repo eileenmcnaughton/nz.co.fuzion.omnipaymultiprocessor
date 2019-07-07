2262033086af0c23c4fc38ca348aebbfcfb9f80b
eileen <emcnaughton@wikimedia.org>
2019-07-07 12:21:12 +1200

    Fix bug where Omnipay was breaking on multiple domains

--- api/v3/Job/ProcessRecurring.php
+++ api/v3/Job/ProcessRecurring.php
@@ -9,7 +9,7 @@
  * @throws CiviCRM_API3_Exception
  */
 function civicrm_api3_job_process_recurring($params) {
-  $omnipayProcessors = civicrm_api3('PaymentProcessor', 'get', array('class_name' => 'Payment_OmnipayMultiProcessor'));
+  $omnipayProcessors = civicrm_api3('PaymentProcessor', 'get', ['class_name' => 'Payment_OmnipayMultiProcessor', 'domain_id' => CRM_Core_Config::domainID()]);
   $recurringPayments = civicrm_api3('ContributionRecur', 'get', array(
     'next_sched_contribution_date' => ['BETWEEN' => [date('Y-m-d 00:00:00'), date('Y-m-d 12:59:59')]],
     'payment_processor_id' => array('IN' => array_keys($omnipayProcessors['values'])),
