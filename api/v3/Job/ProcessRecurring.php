4a9e428c1a53b38b4f8b0dada36e4dbb95342bc2
Carlos Capote <carloscapote@thirdsectordesign.org>
2020-01-27 17:42:25 +0000

    Changes the job so it considers continuous authority repeated payments

--- api/v3/Job/ProcessRecurring.php
+++ api/v3/Job/ProcessRecurring.php
@@ -35,29 +35,51 @@ function civicrm_api3_job_process_recurring($params) {
         'is_email_receipt' => FALSE,
       ));
 
-      $payment = civicrm_api3('PaymentProcessor', 'pay', array(
-        'amount' => $originalContribution['total_amount'],
-        'currency' => $originalContribution['currency'],
-        'payment_processor_id' => $paymentProcessorID,
-        'contributionID' => $pending['id'],
-        'contribution_id' => $pending['id'],
-        'contactID' => $originalContribution['contact_id'],
-        'description' => ts('Repeat payment, original was ' . $originalContribution['id']),
-        'token' => civicrm_api3('PaymentToken', 'getvalue', [
-          'id' => $recurringPayment['payment_token_id'],
-          'return' => 'token',
-        ]),
-        'payment_action' => 'purchase',
-      ));
-      $payment = reset($payment['values']);
+      if (!is_continuous_authority($omnipayProcessors['values'][$paymentProcessorID])) {
+        $payment = civicrm_api3('PaymentProcessor', 'pay', array(
+          'amount' => $originalContribution['total_amount'],
+          'currency' => $originalContribution['currency'],
+          'payment_processor_id' => $paymentProcessorID,
+          'contributionID' => $pending['id'],
+          'contribution_id' => $pending['id'],
+          'contactID' => $originalContribution['contact_id'],
+          'description' => ts('Repeat payment, original was ' . $originalContribution['id']),
+          'token' => civicrm_api3('PaymentToken', 'getvalue', [
+            'id' => $recurringPayment['payment_token_id'],
+            'return' => 'token',
+          ]),
+          'payment_action' => 'purchase',
+        ));
+        $payment = reset($payment['values']);
 
-      civicrm_api3('Contribution', 'completetransaction', array(
-        'id' => $pending['id'],
-        'trxn_id' => $payment['trxn_id'],
-        'payment_processor_id' => $paymentProcessorID,
-      ));
-      $result['success']['ids'] = $recurringPayment['id'];
+        civicrm_api3('Contribution', 'completetransaction', array(
+          'id' => $pending['id'],
+          'trxn_id' => $payment['trxn_id'],
+          'payment_processor_id' => $paymentProcessorID,
+        ));
+        $result['success']['ids'] = $recurringPayment['id'];
+      } else {
+        $payment = civicrm_api3('PaymentProcessor', 'pay', array(
+          'amount' => $originalContribution['total_amount'],
+          'currency' => $originalContribution['currency'],
+          'payment_processor_id' => $paymentProcessorID,
+          'contributionID' => $pending['id'],
+          'contribution_id' => $pending['id'],
+          'original_contribution_trxn_id' => $originalContribution['trxn_id'],
+          'continuous_authority_repeat' => TRUE,
+          'contactID' => $originalContribution['contact_id'],
+          'description' => ts('Repeat payment, original was ' . $originalContribution['id']),
+          'payment_action' => 'purchase',
+        ));
+        $payment = reset($payment['values']);
 
+        civicrm_api3('Contribution', 'completetransaction', array(
+          'id' => $pending['id'],
+          'trxn_id' => $payment['trxn_id'],
+          'payment_processor_id' => $paymentProcessorID,
+        ));
+        $result['success']['ids'] = $recurringPayment['id'];
+      }
     }
     catch (CiviCRM_API3_Exception $e) {
       // Failed - what to do?
@@ -83,4 +105,45 @@ function civicrm_api3_job_process_recurring($params) {
  * @return array
  */
 function _civicrm_api3_job_process_recurring_spec(&$params) {
+}
+
+function is_continuous_authority($paymentProcessor) {
+  $paymentProcessorType = civicrm_api3('PaymentProcessorType', 'get', array(
+    'id' => $paymentProcessor['payment_processor_type_id'],
+    'sequential' => 1,
+  ));
+
+  $property = get_processor_type_property($paymentProcessorType['values'][0]['name'], 'continuous_authority');
+
+  if($property['found']) {
+    return $property['value'];
+  } else {
+    return FALSE;
+  }
+}
+
+function get_processor_type_by_name($processorTypeName, $entity) {
+  if($entity['entity'] != 'payment_processor_type') { return FALSE; }
+  if(!array_key_exists('params', $entity)) { return FALSE; }
+  if(!array_key_exists('name', $entity['params'])) { return FALSE; }
+  return $entity['params']['name'] == $processorTypeName;
+}
+
+function get_processor_type_metadata($processorTypeName) {
+  $entities = [];
+  omnipaymultiprocessor_civicrm_managed($entities);
+  $filter_by_name = function($entity) use($processorTypeName) {
+    return get_processor_type_by_name($processorTypeName, $entity);
+  };
+  return array_values(array_filter($entities, $filter_by_name))[0];
+}
+
+function get_processor_type_property($processorTypeName, $propertyName) {
+  $processorTypeMetadata = get_processor_type_metadata($processorTypeName);
+  if(!array_key_exists('metadata', $processorTypeMetadata)) { return array('found' => FALSE); }
+  if(!array_key_exists($propertyName, $processorTypeMetadata['metadata'])) { return array('found' => FALSE); }
+  return array(
+    'found' => TRUE,
+    'value' => $processorTypeMetadata['metadata'][$propertyName],
+  );
 }
