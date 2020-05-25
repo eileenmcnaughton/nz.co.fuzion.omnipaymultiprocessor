b3c05df7eca34e1bf1510f05b82e831b08715c79
eileen <emcnaughton@wikimedia.org>
2020-05-25 16:12:19 +1200

    Code re-format

--- api/v3/Job/ProcessRecurring.php
+++ api/v3/Job/ProcessRecurring.php
@@ -10,32 +10,32 @@
  */
 function civicrm_api3_job_process_recurring($params) {
   $omnipayProcessors = civicrm_api3('PaymentProcessor', 'get', ['class_name' => 'Payment_OmnipayMultiProcessor', 'domain_id' => CRM_Core_Config::domainID()]);
-  $recurringPayments = civicrm_api3('ContributionRecur', 'get', array(
+  $recurringPayments = civicrm_api3('ContributionRecur', 'get', [
     'next_sched_contribution_date' => ['BETWEEN' => [date('Y-m-d 00:00:00'), date('Y-m-d 23:59:59')]],
-    'payment_processor_id' => array('IN' => array_keys($omnipayProcessors['values'])),
-    'contribution_status_id' => array('IN' => array('In Progress', 'Pending', 'Overdue')),
-    'options' => array('limit' => 0),
-  ));
+    'payment_processor_id' => ['IN' => array_keys($omnipayProcessors['values'])],
+    'contribution_status_id' => ['IN' => ['In Progress', 'Pending', 'Overdue']],
+    'options' => ['limit' => 0],
+  ]);
 
-  $result = array();
+  $result = [];
   foreach ($recurringPayments['values'] as $recurringPayment) {
     $paymentProcessorID = $recurringPayment['payment_processor_id'];
     try {
-      $originalContribution = civicrm_api3('Contribution', 'getsingle', array(
+      $originalContribution = civicrm_api3('Contribution', 'getsingle', [
         'contribution_recur_id' => $recurringPayment['id'],
-        'options' => array('limit' => 1),
+        'options' => ['limit' => 1],
         'is_test' => CRM_Utils_Array::value('is_test', $recurringPayment['is_test']),
         'contribution_test' => CRM_Utils_Array::value('is_test', $recurringPayment['is_test']),
-      ));
+      ]);
       $result[$recurringPayment['id']]['original_contribution'] = $originalContribution;
-      $pending = civicrm_api3('Contribution', 'repeattransaction', array(
+      $pending = civicrm_api3('Contribution', 'repeattransaction', [
         'original_contribution_id' => $originalContribution['id'],
         'contribution_status_id' => 'Pending',
         'payment_processor_id' => $paymentProcessorID,
         'is_email_receipt' => FALSE,
-      ));
+      ]);
 
-      $payment = civicrm_api3('PaymentProcessor', 'pay', array(
+      $payment = civicrm_api3('PaymentProcessor', 'pay', [
         'amount' => $originalContribution['total_amount'],
         'currency' => $originalContribution['currency'],
         'payment_processor_id' => $paymentProcessorID,
@@ -48,25 +48,28 @@ function civicrm_api3_job_process_recurring($params) {
           'return' => 'token',
         ]),
         'payment_action' => 'purchase',
-      ));
+      ]);
       $payment = reset($payment['values']);
 
-      civicrm_api3('Contribution', 'completetransaction', array(
+      civicrm_api3('Contribution', 'completetransaction', [
         'id' => $pending['id'],
         'trxn_id' => $payment['trxn_id'],
         'payment_processor_id' => $paymentProcessorID,
-      ));
+      ]);
       $result['success']['ids'] = $recurringPayment['id'];
 
     }
     catch (CiviCRM_API3_Exception $e) {
       // Failed - what to do?
-      civicrm_api3('ContributionRecur', 'create', array(
+      civicrm_api3('ContributionRecur', 'create', [
         'id' => $recurringPayment['id'],
         'failure_count' => $recurringPayment['failure_count'] + 1,
-      ));
-      civicrm_api3('Contribution', 'create', array(
-        'id' => $pending['id'], 'contribution_status_id' => 'Failed', 'debug' => $params['debug'])
+      ]);
+      civicrm_api3('Contribution', 'create', [
+          'id' => $pending['id'],
+          'contribution_status_id' => 'Failed',
+          'debug' => $params['debug'],
+        ]
       );
       $result[$recurringPayment['id']]['error'] = $e->getMessage();
       $result['failed']['ids'] = $recurringPayment['id'];
