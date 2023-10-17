720e235b7a8579cb92c31dd177fd20eccfcc7bb1
Bastien Ho <bastien@avecnous.eu>
2023-10-17 08:59:41 +0200

    Factorize *serialize functions

--- CRM/Core/Payment/OmnipayMultiProcessor.php
+++ CRM/Core/Payment/OmnipayMultiProcessor.php
@@ -82,7 +82,18 @@ class CRM_Core_Payment_OmnipayMultiProcessor extends CRM_Core_Payment_PaymentExt
    * @var \Civi\Payment\PropertyBag
    */
   protected $propertyBag;
-
+  
+  /**
+   * For PHP8.1
+   * https://www.php.net/manual/en/language.oop5.magic.php#object.serialize
+   * @return array
+   */
+  public function __serialize(): array {
+    $data = (object) get_object_vars($this);
+    $this->cleanupObjectForSerialization($data, TRUE);
+    return (array) $data;
+  }
+  
   /**
    * Serialize, first removing gateway
    *
@@ -91,17 +102,7 @@ class CRM_Core_Payment_OmnipayMultiProcessor extends CRM_Core_Payment_PaymentExt
    * @return string
    */
   public function serialize(): string {
-    $this->cleanupClassForSerialization(TRUE);
-    return serialize(get_object_vars($this));
-  }
-
-  /**
-   * For PHP8.1
-   * https://www.php.net/manual/en/language.oop5.magic.php#object.serialize
-   */
-  public function __serialize(): array {
-    //$this->cleanupClassForSerialization(TRUE);
-    return get_object_vars($this);
+    return serialize($this->serialize());
   }
 
   /**
