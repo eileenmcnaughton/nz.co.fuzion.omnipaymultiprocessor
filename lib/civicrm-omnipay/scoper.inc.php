1e63c99db63559ae3fbfc4b339a136f79754a973
Tim Otten <totten@civicrm.org>
2026-03-10 22:33:54 -0700

    Simplify deps. Add patchers

--- lib/civicrm-omnipay/scoper.inc.php
+++ lib/civicrm-omnipay/scoper.inc.php
@@ -1,5 +1,36 @@
 <?php declare(strict_types = 1);
 
+$scopeFilters = new class() {
+
+  /**
+   * Convert "Omnipay\Foo\Bar" to CiviOmniPay\Omnipay\Foo\Bar::CLASS
+   *
+   * @param string $originalNamespace
+   * @param string $prefix
+   * @param string $content
+   * @return array|string|string[]|null
+   */
+  public function preferClassConstant(string $originalNamespace, string $prefix, string &$content) {
+    $content = preg_replace_callback(';\'(\\\*)' . $originalNamespace .'(\\\+[^\']+)\';', function ($matches) use ($prefix, $originalNamespace) {
+      return $matches[1] . $prefix . '\\' . $originalNamespace . $matches[2] . '::CLASS';
+    }, $content);
+  }
+
+  /**
+   * Convert '\Omnipay\\' to '\CiviOmniPay\\Omnipay\\'
+   * @param string $originalNamespace
+   * @param string $prefix
+   * @param string $content
+   * @return void
+   */
+  public function replaceFragmentaryNamespace(string $originalNamespace, string $prefix, string &$content) {
+    $content = preg_replace_callback(';\'(\\\*)'. $originalNamespace . '(\\\*);', function ($matches) use ($prefix, $originalNamespace) {
+      return "'" . $matches[1] . $prefix . '\\' . $originalNamespace . $matches[2];
+    }, $content);
+  }
+
+};
+
 /**
  * @link https://github.com/humbug/php-scoper/blob/main/docs/configuration.md
  */
@@ -8,13 +39,15 @@ return [
   'exclude-namespaces' => ['Psr\\Log'],
   'exclude-functions' => ['pathload'],
   'patchers' => [
-    static function (string $filePath, string $prefix, string $content): string {
+    static function (string $filePath, string $prefix, string $content) use ($scopeFilters): string {
       $filePath = str_replace(DIRECTORY_SEPARATOR, '/', $filePath);
+
       if ($filePath === 'vendor/omnipay/common/src/Common/Helper.php') {
-        // Convert '\Omnipay\\' to '\CiviOmniPay\\Omnipay\\'
-        $content = preg_replace_callback(';\'(\\\*)Omnipay(\\\*)\';', function ($matches) use ($prefix) {
-          return "'" . $matches[1] . $prefix . '\\\\Omnipay' . $matches[2] . "'";
-        }, $content);
+        $scopeFilters->replaceFragmentaryNamespace('Omnipay', $prefix, $content);
+      }
+
+      if ($filePath === 'vendor/omnipay/paypal/src/RestGateway.php') {
+        $scopeFilters->preferClassConstant('Omnipay', $prefix, $content);
       }
       return $content;
     },
