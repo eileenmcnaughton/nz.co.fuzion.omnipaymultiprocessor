489ab40a37cb37a5af2dbb006537c9be86093437
Tim Otten <totten@civicrm.org>
2026-03-10 21:14:03 -0700

    civicrm-omnipay - Don't need so many "replaces". Just use Guzzle 7.

--- lib/civicrm-omnipay/scoper.inc.php
+++ lib/civicrm-omnipay/scoper.inc.php
@@ -5,6 +5,18 @@
  */
 return [
   'prefix' => 'CiviOmniPay',
-  'exclude-namespaces' => ['Psr'],
+  'exclude-namespaces' => ['Psr\\Log'],
   'exclude-functions' => ['pathload'],
+  'patchers' => [
+    static function (string $filePath, string $prefix, string $content): string {
+      $filePath = str_replace(DIRECTORY_SEPARATOR, '/', $filePath);
+      if ($filePath === 'vendor/omnipay/common/src/Common/Helper.php') {
+        // Convert '\Omnipay\\' to '\CiviOmniPay\\Omnipay\\'
+        $content = preg_replace_callback(';\'(\\\*)Omnipay(\\\*)\';', function ($matches) use ($prefix) {
+          return "'" . $matches[1] . $prefix . '\\\\Omnipay' . $matches[2] . "'";
+        }, $content);
+      }
+      return $content;
+    },
+  ],
 ];
